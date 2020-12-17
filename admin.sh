424f0deb74835578ac44fbff95fbdc8f7be2cd24
TechupBusiness <info@techupbusiness.com>
2020-12-17 23:06:11 +0100

    [BUGFIX][system] admin.sh did not work

--- admin.sh
+++ admin.sh
@@ -5,23 +5,25 @@ source "system/lib.sh"
 # Check for access to control docker
 SUDO=''
 if (( $EUID != 0 )); then
-    SUDO='sudo '
+    SUDO='sudo'
 fi
 
-cd "system/configuration"
+pathServices="system/services"
 
 # get all system services together when starting (exclude configuration folder)
-SERVICES="-f \"../docker-compose.yml\""
-for service in ../services/*; do
-    if [[ -d "$service" ]]; then
-      serviceName=$(basename "$service")
-      if [[ "$serviceName" == "$1" ]] && [[ -z "$2" ]]; then
-        editEnv "../services/$serviceName/template.env" ".env" "interactive" "$serviceName"
-        runScript "../services/$serviceName" "Setup" "multiproject-system" "$serviceName"
-        exit
-      fi
-      SERVICES="$SERVICES -f \"../$serviceName/docker-compose.yml\""
+SERVICES="-f $pathServices/docker-compose.yml"
+for service in system/services/*; do
+  if [[ -d "$service" ]]; then
+    serviceName=$(basename "$service")
+    if [[ "$serviceName" == "$1" ]] && [[ -z "$2" ]]; then
+      editEnv "$pathServices/$serviceName/template.env" "system/configuration/.env" "interactive" "$serviceName"
+      runScript "$pathServices/$serviceName" "Setup" "multiproject" "$serviceName"
+      exit
+    else
+      runScript "$pathServices/$serviceName" "Build" "multiproject" "$serviceName"
+      SERVICES="$SERVICES -f $pathServices/$serviceName/docker-compose.yml"
     fi
+  fi
 done
 
-$SUDO docker-compose -p "multiproject-system" $SERVICES $@
+$SUDO docker-compose --project-directory "system/configuration" -p "multiproject" $SERVICES $@
