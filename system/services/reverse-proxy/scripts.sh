6a81063fe9c0bf260cd6cf8c66b1863c94aa04d9
TechupBusiness <info@techupbusiness.com>
2020-12-17 23:06:51 +0100

    [BUGFIX][system] reverse-proxy setup script had wrong paths

--- system/services/reverse-proxy/scripts.sh
+++ system/services/reverse-proxy/scripts.sh
@@ -2,7 +2,7 @@
 
 reverse-proxySetup() {
   pathConfig="system/configuration"
-  pathProxy="system/reverse-proxy"
+  pathProxy="system/services/reverse-proxy"
 
   echo "Checking if reverse-proxy is configured..."
 
@@ -13,11 +13,11 @@ reverse-proxySetup() {
       echo "SKIPPED - Configuration for reverse-proxy already exists (acme.json)."
   fi
 
-  echo "Checking reverse-proxy configuration (acme.json)..."
+  echo "Checking reverse-proxy configuration permissions (acme.json)..."
   if [[ -f "$pathConfig/acme.json" ]] && [[ $(stat --format '%a' "$pathConfig/acme.json") != "600" ]]; then
-      chmod 600 "$pathConfig/acme.json"
-      echo "Set permission of acme.json to 600."
+    chmod 600 "$pathConfig/acme.json"
+    echo "Set permission of acme.json to 600."
+  else
+    echo "SKIPPED - Permissions for configuration are set properly (600)."
   fi
-  editEnv "$pathProxy/template.env" "$pathConfig/.env" "interactive" "reverseproxy"
-  echo "DONE - Configured reverse-proxy configuration"
 }
