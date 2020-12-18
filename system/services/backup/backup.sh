162de2a8313d0927d44ae11671d8a6e542ddbf86
TechupBusiness <info@techupbusiness.com>
2020-12-18 11:36:13 +0100

    [FEATURE][system] Color backup logs for easier reading

--- system/services/backup/backup.sh
+++ system/services/backup/backup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 # runs daily at 2am
 
+# Text colors
+TEXT_BG_BLUE="\e[44m"
+TEXT_BG_GREEN="\e[42m"
+TEXT_BG_RED="\e[41m"
+TEXT_BG_DEFAULT="\e[49m"
+TEXT_WHITE="\e[97m"
+TEXT_GREEN="\e[92m"
+TEXT_RED="\e[91m"
+TEXT_BLUE="\e[34m"
+TEXT_BOLD="\e[1m"
+TEXT_CLEAR="\e[0m"
+
 # Outputs date to log
-date
+echo -e "${TEXT_BG_BLUE}${TEXT_BOLD}${TEXT_WHITE}BACKUP STARTED $(date)$TEXT_CLEAR"
 
 # Variables
 PROJECT_FOLDER_PARENTS="applications/backups applications/cronjobs applications/docker-data applications/instance-data applications/logs"
@@ -22,7 +34,7 @@ if [[ ! -z "$BACKUP_ADDITIONAL_FOLDER" ]]; then
     BACKUP_FOLDERS="$BACKUP_FOLDERS $BACKUP_ADDITIONAL_FOLDER"
 fi
 
-# Iterate all projects
+# Iterate all projects (/docker-projects is the root for the multiproject install and contains everything to access readonly)
 PATH_ROOT=/docker-projects
 PATH_PROJECT_FOLDER="$PATH_ROOT/applications/docker-data"
 
@@ -34,6 +46,7 @@ for PATH_PROJECT in $PATH_PROJECT_FOLDER/*; do
     for PROJECT_FOLDER_PARENT in $PROJECT_FOLDER_PARENTS; do
         PATH_PROJECT_FOLDER="$PATH_ROOT/$PROJECT_FOLDER_PARENT/$PROJECT"
         if [[ -d "$PATH_PROJECT_FOLDER" ]]; then
+            echo -e "${TEXT_GREEN}${TEXT_BOLD}BACKING UP $PROJECT/$(basename "$PROJECT_FOLDER_PARENT"):${TEXT_CLEAR} $PATH_PROJECT_FOLDER"
             CMD_RESTIC="restic -r $BACKUP_RESTIC_REPO $O_PARAM backup --tag 'project:$PROJECT' --tag 'folder:$PROJECT_FOLDER_PARENT' $PATH_PROJECT_FOLDER"
             eval $CMD_RESTIC
         fi
@@ -41,6 +54,8 @@ for PATH_PROJECT in $PATH_PROJECT_FOLDER/*; do
 done
 
 for BACKUP_FOLDER in $BACKUP_FOLDERS; do
+    FOLDER=$(basename "$BACKUP_FOLDER")
+    echo -e "${TEXT_GREEN}${TEXT_BOLD}BACKING UP system $FOLDER:${TEXT_CLEAR} $BACKUP_FOLDER"
     CMD_RESTIC="restic -r $BACKUP_RESTIC_REPO $O_PARAM backup --tag 'folder:$BACKUP_FOLDER' $PATH_ROOT/$BACKUP_FOLDER"
     eval $CMD_RESTIC
 done
--- system/services/backup/cleanup.sh
+++ system/services/backup/cleanup.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 # runs weekly at 3am on sunday
 
+# Text colors
+TEXT_BG_BLUE="\e[44m"
+TEXT_BG_GREEN="\e[42m"
+TEXT_BG_RED="\e[41m"
+TEXT_BG_DEFAULT="\e[49m"
+TEXT_WHITE="\e[97m"
+TEXT_GREEN="\e[92m"
+TEXT_RED="\e[91m"
+TEXT_BLUE="\e[34m"
+TEXT_BOLD="\e[1m"
+TEXT_CLEAR="\e[0m"
+
+CONFIG="forget --keep-daily $BACKUP_KEEP_DAILY --keep-weekly $BACKUP_KEEP_WEEKLY --keep-monthly $BACKUP_KEEP_MONTHLY --keep-yearly $BACKUP_KEEP_YEARLY --keep-last 1"
+
 # Outputs date to log
-date
+echo -e "${TEXT_BG_BLUE}${TEXT_BOLD}${TEXT_WHITE}CLEANUP STARTED $(date):${TEXT_CLEAR}
+${TEXT_BLUE}CONFIGURATION:${TEXT_CLEAR} $CONFIG"
 
 # Repo params
 O_PARAM=""
@@ -10,5 +25,5 @@ if [[ ! -z "$BACKUP_RESTIC_SSH_PASSWORD" ]]; then
     O_PARAM="-o sftp.command='sshpass -p $BACKUP_RESTIC_SSH_PASSWORD ssh $BACKUP_RESTIC_SSH_USER@$BACKUP_RESTIC_SSH_HOST -s sftp'"
 fi
 
-CMD_RESTIC="restic -r $BACKUP_RESTIC_REPO $O_PARAM forget --keep-daily $BACKUP_KEEP_DAILY --keep-weekly $BACKUP_KEEP_WEEKLY --keep-monthly $BACKUP_KEEP_MONTHLY --keep-yearly $BACKUP_KEEP_YEARLY --keep-last 1"
+CMD_RESTIC="restic -r $BACKUP_RESTIC_REPO $O_PARAM $CONFIG"
 eval $CMD_RESTIC
